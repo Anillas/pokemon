7300d486fc008c4f92c5d5bd219c107be2e07c41
Anillas <furiousanillas@gmail.com>
2020-05-16 21:18:22 -0300

    adição de arquivos que serão removidos posteriormente

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" type="image/png" href="icon.png">
+	<link rel="stylesheet" type="text/css" href="FXY78E3NK5KJD7683HJK/B/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="FXY78E3NK5KJD7683HJK/F/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="cascata.css">
+	<title>Sistema</title>
+</head>
+<body>
+	<div class="menu">
+		<h2>Nilza - NomePágina</h2>
+		<div class="pages">
+			<div>
+				<button class="btn btn-sm">Pedidos</button>
+				<button class="btn btn-sm">Financeiro</button>
+				<button class="btn btn-sm"><i class="far fa-sun" id="sun"></i><i class="far fa-moon"></i></button>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
